c7ccfe5c4fa2d35f21fe289fd0750282fb9b7b5a
Lucas Pagac <lucaspagac0@gmail.com>
2021-01-19 11:55:38 -0800

    after code review

--- src/EightBall.js
+++ src/EightBall.js
@@ -24,8 +24,10 @@ function EightBall({answers}) {
     textAlign: "center",
   };
 
+  // Docstring
   function handleClick() {
     const index = Math.floor(Math.random() * answers.length);
+    // Destructure
     const color = answers[index].color;
     const msg = answers[index].msg;
     setBackgroundColor(color);
@@ -38,6 +40,33 @@ function EightBall({answers}) {
     </div>
   )
 }
+
+const ANSWERS = [
+  { msg: "It is certain.", color: "green" },
+  { msg: "It is decidedly so.", color: "green" },
+  { msg: "Without a doubt.", color: "green" },
+  { msg: "Yes - definitely.", color: "green" },
+  { msg: "You may rely on it.", color: "green" },
+  { msg: "As I see it, yes.", color: "green" },
+  { msg: "Most likely.", color: "green" },
+  { msg: "Outlook good.", color: "green" },
+  { msg: "Yes.", color: "green" },
+  { msg: "Signs point to yes.", color: "goldenrod" },
+  { msg: "Reply hazy, try again.", color: "goldenrod" },
+  { msg: "Ask again later.", color: "goldenrod" },
+  { msg: "Better not tell you now.", color: "goldenrod" },
+  { msg: "Cannot predict now.", color: "goldenrod" },
+  { msg: "Concentrate and ask again.", color: "goldenrod" },
+  { msg: "Don't count on it.", color: "red" },
+  { msg: "My reply is no.", color: "red" },
+  { msg: "My sources say no.", color: "red" },
+  { msg: "Outlook not so good.", color: "red" },
+  { msg: "Very doubtful.", color: "red" },
+];
+
+EightBall.defaultProps = {
+  answers: ANSWERS
+};
 
 
 export default EightBall;
